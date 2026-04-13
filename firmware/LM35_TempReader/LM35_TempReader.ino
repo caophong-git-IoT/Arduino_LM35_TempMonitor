c49b3aa946f737fd80e6a91baefc30253a848604
Letrungbinh <lebinh121202@gmail.com>
2026-04-13 20:02:38 +0700

    feat: them kenh A2

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,27 +1,28 @@
-/*
- * Du an: Doc nhiet do LM35 va gui qua Serial
- * Mon: Mang Cam Bien - Tuan 03
- * Version: 2 kenh (A0, A1)
- */
-
-int adcValues[2];
-float nhietDo[2];
-char chuoi[30];
+int sensorPin1 = A0;
+int sensorPin2 = A1;
+int sensorPin3 = A2;
 
 void setup() {
   Serial.begin(9600);
   delay(100);
+  Serial.println("Temp1,Temp2,Temp3"); // CSV header
 }
 
 void loop() {
-  adcValues[0] = analogRead(A0);
-  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
-  
-  adcValues[1] = analogRead(A1);
-  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
-  
-  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
-  Serial.print(chuoi);
-  
-  delay(100);
+  int adcValue1 = analogRead(sensorPin1);
+  int adcValue2 = analogRead(sensorPin2);
+  int adcValue3 = analogRead(sensorPin3);
+
+  float temp1 = (adcValue1 * 500.0) / 1023.0;
+  float temp2 = (adcValue2 * 500.0) / 1023.0;
+  float temp3 = (adcValue3 * 500.0) / 1023.0;
+
+  // Xuất CSV: temp1,temp2,temp3
+  Serial.print(temp1);
+  Serial.print(",");
+  Serial.print(temp2);
+  Serial.print(",");
+  Serial.println(temp3);
+
+  delay(1000);
 }
